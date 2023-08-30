1e28d5b39366402d7af88b4cca7d5dc462770f28
jorgechvzpon <jorgechvzpon@gmail.com>
2023-08-30 22:06:46 +0000

    Fix the urls

--- common/header.php
+++ common/header.php
@@ -1,14 +1,14 @@
-<img src="/phpmotors/images/site/logo.png" alt="PHP Motors Logo Icon">
+<img src="../images/site/logo.png" alt="PHP Motors Logo Icon">
 <div class="header-div">
-    <a href="/phpmotors/search/" title="Search PHP Motors">&#x1F50D;</a>
+    <a href="../search/" title="Search PHP Motors">&#x1F50D;</a>
     <?php
     if (isset($_SESSION['loggedin'])) {
         $messageHeader = $_SESSION['clientData']['clientFirstname'];
-        echo "<a class='btn_myaccount' href='/phpmotors/accounts/index.php'><p>Welcome $messageHeader</p></a>";
+        echo "<a class='btn_myaccount' href='../accounts/index.php'><p>Welcome $messageHeader</p></a>";
         echo "<p>|</p>";
-        echo "<a class='btn_myaccount' href='/phpmotors/accounts/index.php?action=logout'><p>Logout</p></a>";
+        echo "<a class='btn_myaccount' href='../accounts/index.php?action=logout'><p>Logout</p></a>";
     } else {
-        echo "<a class='btn_myaccount' href='/phpmotors/accounts/index.php?action=myaccount'><p>My Account</p></a>";
+        echo "<a class='btn_myaccount' href='../accounts/index.php?action=myaccount'><p>My Account</p></a>";
     }
     ?>
 </div>
